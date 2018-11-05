409fb6ff475f622724beff9fe85b4807016fa804
Jakub Gabała <jakub.gabala@gmail.com>
2018-11-05 16:16:10 +0100

    Added Python docstrings, several cosmetic changes and new function (m_process())

--- robotics.py
+++ robotics.py
@@ -3,8 +3,17 @@ import mpmath as mp
 import typing
 import numbers
 
-# defines a SO(4) matrix defining coordinates in space
-def matrix_coord(x,y,z):
+
+"""
+	Defines a SO(4) matrix defining coordinates in space.
+	
+		:param x: X-coordinate
+		:param y: Y-coordinate
+		:param z: Z-coordinate
+		:return: evaluated matrix
+		:rtype: object inheriting from sym.matrices.MatrixBase
+"""
+def m_coord(x,y,z):
 	# checks if parameters ar numbers
 	assert isinstance(z, numbers.Number) and isinstance(y, numbers.Number) and isinstance(z, numbers.Number), "All parameters should be numbers."
 	
@@ -15,8 +24,20 @@ def matrix_coord(x,y,z):
 	[ 0,0,0,1,]
 	]), 5, chop=True)
 
-# defines a rotation matrix in Denavit-Hartenberg notation
-def matrix_rot(axis, variable, use_degrees=True):
+
+"""
+	Defines a rotation matrix in Denavit-Hartenberg notation.
+
+		:param axis: one of Cartesian coordinate system axes (x, y, z)
+		:param variable: variable defining rotation
+		:param use_degrees: uses degrees if True, uses rads otherwise
+		:type axis: char
+		:type variable: numbers.Number or sympy.Symbol
+		:type use_degrees: bool
+		:return: evaluated matrix
+		:rtype: object inheriting from sym.matrices.MatrixBase
+"""
+def m_rot(axis, variable, use_degrees=True):
 	# check if axis provided is correct
 	ax = axis.lower()
 	assert ax in ['x','y','z'], "Invalid axis provided."
@@ -49,8 +70,18 @@ def matrix_rot(axis, variable, use_degrees=True):
 	[0,0,0,1,]
 	]), 5, chop=True)
 
-# defines a translation matrix in Denavit-Hartenberg notation
-def matrix_trans(axis, variable, use_degrees=True):
+
+"""
+	Defines a translation matrix in Denavit-Hartenberg notation.
+
+		:param axis: one of Cartesian coordinate system axes (x, y, z)
+		:param variable: variable defining translation
+		:type axis: char
+		:type variable: numbers.Number or sympy.Symbol
+		:return: evaluated matrix
+		:rtype: object inheriting from sym.matrices.MatrixBase
+"""
+def m_trans(axis, variable):
 	# check if axis provided is correct
 	ax = axis.lower()
 	assert ax in ['x','y','z'], "Invalid axis provided."
@@ -58,11 +89,7 @@ def matrix_trans(axis, variable, use_degrees=True):
 	# checks if variable is a Symbol or a number
 	assert isinstance(variable, sym.Symbol) or isinstance(variable, numbers.Number), "'variable' should be a sympy.Symbol or a number."
 
-	# convert to degrees if set to true
-	if not isinstance(variable, sym.Symbol) and use_degrees:
-		var = mp.radians(variable)
-	else:
-		var = variable
+	var = variable
 
 	return sym.N(sym.Matrix([
 	[ 1,0,0,{'x':var}.get(ax, 0) ],
@@ -71,13 +98,43 @@ def matrix_trans(axis, variable, use_degrees=True):
 	[ 0,0,0,1,]
 	]), 5, chop=True)
 
-# defines a Denavit-Hartenberg transformation
-def transformation(Rz, Tz, Tx, Rx, use_degrees=True):
-	return matrix_rot('z', Rz, use_degrees) * matrix_trans('z', Tz, use_degrees) * matrix_trans('x', Tx, use_degrees) * matrix_rot('x', Rx, use_degrees)
 
-# evaluates the matrix
-# to perform multiple substitutions at once, pass a list of (old, new) pairs to subs
-def matrix_evaluate(matrix, subs):
+"""
+	Defines a transformation in Denavit-Hartenberg notation.
+
+		:param Rz: variable used in z-rotation matrix
+		:param Tz: variable used in z-translation matrix
+		:param Tx: variable used in x-translation matrix
+		:param Rx: variable used in x-rotation matrix
+		:param use_degrees: uses degrees if True, uses rads otherwise
+		:param simplify: simplifies expression if True, does not simplify otherwise
+		:type Rz: numbers.Number or sympy.Symbol
+		:type Tz: numbers.Number or sympy.Symbol
+		:type Tx: numbers.Number or sympy.Symbol
+		:type Rx: numbers.Number or sympy.Symbol
+		:type use_degrees: bool
+		:type simplify: bool
+		:return: evaluated matrix
+		:rtype: object inheriting from sym.matrices.MatrixBase
+"""
+def m_transformation(Rz, Tz, Tx, Rx, use_degrees=True, simplify=True):
+	K = m_rot('z', Rz, use_degrees) * m_trans('z', Tz, use_degrees) * m_trans('x', Tx, use_degrees) * m_rot('x', Rx, use_degrees)
+	if simplify:
+		K = sym.simplify(K)
+	return K
+
+
+"""
+	Evaluates the matrix over chosen variable(s).
+	
+	:param matrix: evaluated matrix
+	:param subs: list of symbol-value tuples
+	:type matrix: object inheriting from sym.matrices.MatrixBase
+	:type subs: list of tuples
+	:return: evaluated matrix
+	:rtype: object inheriting from sym.matrices.MatrixBase
+"""
+def m_evaluate(matrix, subs):
 	# check if subs is a list of tuples with non-zero length
 	assert isinstance(subs, typing.List) and len(subs) > 0 and isinstance(subs[0], typing.Tuple), "'subs' must be a list of tuples with non-zero length."
 	# check if one would like to substitute for existing elements
@@ -86,3 +143,25 @@ def matrix_evaluate(matrix, subs):
 	assert issubclass(matrix.__class__, sym.matrices.MatrixBase), "Cannot evaluate non-matrix."
 
 	return sym.N(matrix.subs(subs), 5, chop=True)
+
+
+"""
+	Processes the matrix.
+	
+	:param matrix: processed matrix
+	:param simplify: simplifies expression if True, does not simplify otherwise
+	:type matrix: object inheriting from sym.matrices.MatrixBase
+	:type simplify: bool
+	:return: evaluated matrix
+	:rtype: object inheriting from sym.matrices.MatrixBase
+"""
+def m_process(matrix, simplify=True):
+	# check if one would like to evaluate a matrix
+	assert issubclass(matrix.__class__, sym.matrices.MatrixBase), "Cannot evaluate non-matrix."
+
+	M = matrix
+
+	if simplify:
+		M = sym.simplify(M)
+	
+	return M
--- script.py
+++ script.py
@@ -1,16 +1,22 @@
-from sympy import Symbol, latex
+import sympy as sym
 import robotics as robo
 
-v = robo.matrix_coord(0,0,0)
 
-q1 = Symbol('q_1')
-q2 = Symbol('q_2')
-q3 = Symbol('q_3')
+q1, q2, l1, l2 = sym.symbols("q_1 q_2 l_1 l_2")
 
-A1 = robo.transformation(q1,0,0,90)
-A2 = robo.transformation(0,q2,0,0)
+A10 = robo.m_transformation(q1,0,l1,0)
+A21 = robo.m_transformation(q2,0,l2,0)
 
-v_f = v * A1 * A2
-v_fs = robo.matrix_evaluate(v_f, [(q1, 90)])
+# v = robo.m_coord(0,0,0)
+# v_f = v * A10 * A21
+# v_fs = robo.m_evaluate(v_f, [(q1, 90)])
 
-print(latex(v_fs))
+K = robo.m_process(A10 * A21)
+
+print(sym.latex(K))
+
+
+# x = Symbol('x')
+# f = cos(x) + x**2
+# print(f)
+# print(latex(Derivative(f,x,1)))
