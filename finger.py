d2c6f4fce3b8079f6b4bf3e06a5e4b3bca689484
Jakub Gabała <jakub.gabala@gmail.com>
2018-11-17 16:51:08 +0100

    .

---
+++ finger.py
@@ -0,0 +1,25 @@
+import os
+import pickle_tools as pt
+
+class Finger:
+
+	directory = ''
+
+	def __init__(self, number: int, njoints: int, fdir: str = directory):
+		self.directory = fdir
+		self.fNumber = number
+
+		assert 'V{0}'.format(self.fNumber) in [x for x in os.listdir(self.directory) if x[0] == 'V'], "There is no data for base of finger number {0}.".format(self.fNumber)
+
+		self.V = pt.pickle_out('V{0}'.format(self.fNumber), self.directory)
+		print("Finger {0}: base coordinates ready".format(self.fNumber))
+
+		# transformation matrices
+		self.K = []
+
+		for i in range(0,njoints):
+			assert 'K{0}{1}'.format(self.fNumber, i) in [x for x in os.listdir(self.directory) if x[0:2] == 'K{0}'.format(self.fNumber)], "There is no data for transformation matrix number {0} of finger {1}.".format(i, self.fNumber)			
+			self.K.append(pt.pickle_out('K{0}{1}'.format(self.fNumber, i), self.directory))
+			print("Finger {2}: transformation {0}->{1} ready.".format(i, i+1, self.fNumber))
+
+		print("Finger {0} ready.".format(self.fNumber))
