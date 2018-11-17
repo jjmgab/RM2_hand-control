827672c6d6e990147b9d0e816e411354f7b359c8
Jakub Gabała <jakub.gabala@gmail.com>
2018-11-17 16:43:53 +0100

    Finger class constructor finished. Performance tests - very satisfying.

--- script.py
+++ script.py
@@ -5,6 +5,8 @@ import numbers
 import os
 import finger as f
 
+import time
+
 """
 	First of all, one should run 'hand_model.py', which defines the model of the hand
 	and saves it to files.
@@ -14,8 +16,17 @@ if __name__ == '__main__':
 	directory = 'pickles'
 	fingers = []
 
+	print("Begin script.")
+
+	t_start = time.time()
+
 	for i in range(0,4):
-		fingers.append(f.Finger(i, directory))
+		fingers.append(f.Finger(i, 4, directory))
+
+	t_end = time.time()
+
+	print("End script.")
+	print("Time elapsed: {0:.5f} seconds".format(t_end - t_start))
 
 	# list_dir = os.listdir(directory)
 	# l_bcoord = [x for x in list_dir if x[0] == 'V']
